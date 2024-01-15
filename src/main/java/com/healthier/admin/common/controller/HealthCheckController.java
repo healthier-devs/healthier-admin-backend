d67fdfa63813702beb7fcd2719a8f1da22f19720
yourzinc <your_zinc@ewhain.net>
2024-01-15 13:55:16 +0900

    chore: add spotless

--- src/main/java/com/healthier/admin/common/controller/HealthCheckController.java
+++ src/main/java/com/healthier/admin/common/controller/HealthCheckController.java
@@ -1,12 +1,11 @@
 package com.healthier.admin.common.controller;
 
+import java.time.LocalDateTime;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/v1")
--- src/main/java/com/healthier/admin/config/OpenApiConfig.java
+++ src/main/java/com/healthier/admin/config/OpenApiConfig.java
@@ -6,11 +6,10 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Arrays;
 
 @Configuration
 public class OpenApiConfig {
@@ -19,10 +18,7 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI openAPI() {
-        Info info =
-                new Info()
-                        .title("Healthier Admin API")
-                        .version("1.0.0");
+        Info info = new Info().title("Healthier Admin API").version("1.0.0");
 
         String jwtSchemeName = "jwtAuth";
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
