c84fb95d5d6ee97fce603608d17a2c691f591162
Jeonghyun Lee <hyeon00@ewhain.net>
2024-01-16 10:44:39 +0900

    chore: 어노테이션 수정

--- src/main/java/com/healthier/admin/common/entity/BaseEntity.java
+++ src/main/java/com/healthier/admin/common/entity/BaseEntity.java
@@ -3,12 +3,11 @@ package com.healthier.admin.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
@@ -19,6 +18,5 @@ public abstract class BaseEntity {
     @Column(updatable = false)
     private LocalDateTime createdDate;
 
-    @LastModifiedDate
-    private LocalDateTime modifiedDate;
+    @LastModifiedDate private LocalDateTime modifiedDate;
 }
--- src/main/java/com/healthier/admin/domain/user/domain/healthInformation/HealthInformation.java
+++ src/main/java/com/healthier/admin/domain/user/domain/healthInformation/HealthInformation.java
@@ -4,12 +4,13 @@ import com.healthier.admin.domain.user.domain.User;
 import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
-@Setter
 @Entity
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class HealthInformation {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
